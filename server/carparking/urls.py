6e5f0f9cb43cc9985092e76f7463d33545394865
david <kdavidmacharia8485@gmail.com>
2025-02-15 16:55:21 +0300

    contact router

--- server/carparking/urls.py
+++ server/carparking/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ParkingSlotViewSet, BookingViewSet, RegisterView, LoginView
+from .views import ParkingSlotViewSet, BookingViewSet, RegisterView, LoginView, ContactViewSet
 
 router = DefaultRouter()
 router.register(r'parking-slots', ParkingSlotViewSet)
 router.register(r'bookings', BookingViewSet)
+router.register(r'contacts', ContactViewSet)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
